3a014c32c68549e97a17a747301c66f8a121b630
Divine Atansi <divineatansi123@gmail.com>
2026-01-05 09:45:21 +0100

    Updating coinpulse

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main className='main-container'>
+      <section className='home-grid'>
+        <div className='coin-overview'>
+            <div className='header'>
+                <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" />
+            </div>
+        </div>
+
+        <p>Trending Coins</p>
+      </section>
+
+      <section className='w-full mt-7 space-y-4'>
+        <p>Categories</p>
+      </section>
+    </main>
   )
 }
 
